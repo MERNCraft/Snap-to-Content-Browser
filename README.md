196660da06b0ea7becd0574a6aa760fa059b6d6c
James Newton <blackslate@lexogram.com>
2025-01-26 19:49:55 +0100

    No element scrollbars; use wheel event; double buffer

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# SMART SCROLL #
+# SMART SCROLL: SNAPS TO CONTENT #
 
 Use a single scroll bar to:
 
-* Scroll a whole page until the text content is reached
-* Make the text context "sticky" and 1 viewport high: scroll that while it is at the top
-* Scroll the whole page again when the text context is scrolled to the end
+* Scroll a whole page until the main content is reached
+* Make the main content "sticky" and 1 viewport high:
+  + Scroll the main content to the end without scrolling the page
+  + Show a non-scrolling aside
+* Scroll the whole page again when the main content is
+  scrolled to the end to show the footer
+  
--- script.js
+++ script.js
@@ -2,6 +2,7 @@
  * script.js
  */
 
+
 const scrollable = document.getElementById("scrollable")
 const header     = document.getElementById("header")
 const article    = document.getElementById("article")
@@ -9,6 +10,12 @@ const footer     = document.getElementById("footer")
 const scrollbar  = document.getElementById("scrollbar")
 const stuff      = document.getElementById("stuff")
 
+
+// Forward wheel event to scrollbar
+window.addEventListener("wheel", useScrollbar)
+function useScrollbar({deltaY}) {
+  scrollbar.scrollTop += deltaY
+}
 
 function initialize() {
   while (scrollable.scrollTop) {
@@ -21,19 +28,18 @@ function initialize() {
   const articleHeight = article.getBoundingClientRect().height
   const articleScroll = article.scrollHeight - articleHeight
 
-  // Make user scroll an extra 10% to reveal the footer
-  const buffer        = 200 + Math.sqrt(articleScroll)
-  console.log("buffer:", buffer)
+  // Make user scroll an extra distance to reveal the footer
+  const buffer        = 200 + Math.sqrt(articleScroll) * 2
   const footScroll    = headerScroll
                       + articleScroll
-                      + buffer
+                      + buffer * 2
   const footerScroll  = footer.scrollHeight
   const stuffHeight = 
     headerScroll
   + articleScroll
   + articleHeight
   + footerScroll
-  + buffer
+  + buffer * 2
 
   stuff.style.height = stuffHeight + "px"
   scrollbar.addEventListener("scroll", smartScroll)
@@ -49,7 +55,7 @@ function initialize() {
       scrollable.scrollTop = headerScroll
 
       // Scroll the article
-      article.scrollTop = scrollTop - headerScroll
+      article.scrollTop = scrollTop - headerScroll - buffer
 
     } else {
       // Scroll the whole scrollable section to show footer
@@ -59,6 +65,8 @@ function initialize() {
 }
 
 
+// Housekeeping
+article.style.fontSize = "1em"
 initialize()
 
 
